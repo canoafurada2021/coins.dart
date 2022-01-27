a9a6fc791a6f75dabc9cbb66be8318025838bff3
Canoa furada =) <88066241+canoafurada2021@users.noreply.github.com>
2022-01-27 18:29:44 -0300

    Update muah.dart

--- lib/muah.dart
+++ lib/muah.dart
@@ -1,59 +1,57 @@
-import 'package:flutter/material.dart';
-
-void main() => runApp(Muah());
-
-class Muah extends StatelessWidget {
-  const Muah({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-       extendBodyBehindAppBar: true,
-        appBar: AppBar(
-          title: const Text('App Bar'),
-          centerTitle: true,
-          leading: BackButton(),
-          actions: [
-            IconButton(
-              icon: Icon(Icons.search),
-              onPressed: () {},
-            )
-          ],
-          backgroundColor: Colors.transparent,
-          elevation: 0,
-        ),
-        drawer: const Drawer(),
-        body: Center(
-          child: Container(
-            decoration: const BoxDecoration(
-              gradient: LinearGradient(
-                  begin: Alignment.topLeft,
-                  end: Alignment.bottomRight,
-                  stops:[0.5, 1],
-                  colors: [Colors.black87, Colors.purple])
-            ),
-            child: Center(
-              child: const Text('Hello World'),
-            ),
-    ),
-        ),
-        bottomNavigationBar: BottomNavigationBar(
-          type: BottomNavigationBarType.fixed,
-          items: const <BottomNavigationBarItem>[
-            BottomNavigationBarItem(
-              icon: Icon(Icons.shopping_bag_rounded),
-              label:'item 1',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.star),
-              label: 'item 2',
-            ),
-          ],
-        ),
-    ),
-
-    );
-
-  }
-}
+import 'package:flutter/material.dart';                                           
+import 'package:flutter_projects/repository/coins_repository.dart';               
+import 'package:intl/intl.dart';                                                  
+                                                                                  
+import '../models/models_coins.dart';                                             
+                                                                                  
+class MoedasPage extends StatefulWidget {                                         
+  const MoedasPage({Key? key}) : super(key: key);                                 
+                                                                                  
+  @override                                                                       
+  State<MoedasPage> createState() => _MoedasPageState();                          
+}                                                                                 
+                                                                                  
+class _MoedasPageState extends State<MoedasPage> {                                
+  @override                                                                       
+  Widget build(BuildContext context) {                                            
+    final tabela = MoedaRepository.tabela;                                        
+    NumberFormat real = NumberFormat.currency(locale: 'pt_BR', name: 'R\$');      
+    List<Moeda> selecionadas = [];                                                
+    return Scaffold(                                                              
+        appBar: AppBar(                                                           
+          title: const Text('Cripto Moedas'),                                     
+          centerTitle: true,                                                      
+        ),                                                                         
+        body: ListView.separated(                                                 
+          itemBuilder: (BuildContext context, int moeda) {                        
+            return ListTile(                                                      
+              shape: const RoundedRectangleBorder(                                
+                borderRadius: BorderRadius.all(Radius.circular(12)),              
+              ),                                                                   
+              leading: SizedBox(                                                  
+                child: Image.asset(tabela[moeda].iconss),                         
+                width: 40,                                                        
+              ),                                                                   
+              title: Text(tabela[moeda].nome, style: const TextStyle(             
+                fontSize: 17,                                                     
+                fontWeight: FontWeight.w500,                                      
+              ),),                                                                 
+              trailing: Text(real.format(tabela[moeda].preco),                    
+              ),                                                                   
+              selected: selecionadas.contains(tabela[moeda]),                     
+              selectedTileColor: Colors.indigo[50],                               
+              onLongPress: () {                                                   
+                setState(() {                                                     
+                 (selecionadas.contains(tabela[moeda]))                           
+                                     ? selecionadas.remove(tabela[moeda])         
+                                     : selecionadas.add(tabela[moeda]);           
+                });                                                               
+              },                                                                  
+            );                                                                     
+          },                                                                      
+          padding: const EdgeInsets.all(16),                                      
+          separatorBuilder: (_, ___) => Divider(),                                
+          itemCount: tabela.length,                                               
+        ));                                                                        
+  }                                                                               
+}                                                                                 
